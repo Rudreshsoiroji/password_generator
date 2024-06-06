9e37ef4f21c918ec2b67c84cdda53ca37122ed5c
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-06 23:43:58 +0530

    added use ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState ,useCallback} from 'react'
+import { useState ,useCallback, useEffect, useRef} from 'react'
 import './App.css'
 
 function App() {
@@ -6,9 +6,33 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharacAllowed] = useState(false)
   const [password, setPassword] = useState('')
+ 
+ const passRef = useRef();
+  
+  const passwordgeneretor = useCallback(()=>{
 
-  const passwordgeneretor() =>{
+let pass = ""
+let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+if (numberAllowed) str +="1234567890"
+if (charAllowed) str += "!@#$%^&*_+"
 
+for (let i = 1; i < lenght; i++) {
+  const char = Math.floor(Math.random() * str.length + 1)
+
+  pass += str.charAt(char)
+  
+}
+
+setPassword(pass)
+  },[charAllowed, numberAllowed, lenght])
+
+  useEffect(()=>{
+    passwordgeneretor()
+  },[charAllowed,numberAllowed,lenght])
+
+  const copyPassword = () =>{
+    window.navigator.clipboard.writeText(password)
+    passRef.current?.select()
   }
 
   return (
@@ -16,8 +40,8 @@ function App() {
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-5 py-4 my-8 bg-gray-800 text-blue-500 ">
         <h1 className='text-white text-center font-bold my-8'>Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden">
-          <input type="text" value={password} className='outline-none w-full py-2 px-3' placeholder='password' readOnly  />
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+          <input type="text" value={password} className='outline-none w-full py-2 px-3' placeholder='password' readOnly ref={passRef}  />
+          <button onClick={copyPassword} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
         </div>
         <div className="flex text-sm gap-x-2 my-10">
           <div className="flex items-center gap-x-1">
